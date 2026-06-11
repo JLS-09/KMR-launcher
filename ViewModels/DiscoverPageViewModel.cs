11b0263a9367564e31de378ec94b6fef3ec4e438
JLS <jules.van.nevel@gmail.com>
2026-06-11 19:56:02 +0200

    fixed mod and author filters

--- ViewModels/DiscoverPageViewModel.cs
+++ ViewModels/DiscoverPageViewModel.cs
@@ -1,5 +1,7 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using KMRLauncherMvvm.Data;
 using KMRLauncherMvvm.Models;
@@ -12,67 +14,30 @@ namespace KMRLauncherMvvm.ViewModels;
 public partial class DiscoverPageViewModel : PageViewModel
 {
     private readonly IModApiService _api;
-    private bool _isFetching;
-
-    public ObservableCollection<Mod> ModList
+    private bool _isLoading;
+    private ObservableCollection<Mod> _modList = [];
+    
+    [ObservableProperty] private ObservableCollection<Mod> _modListFiltered = [];
+    [ObservableProperty] private string _connectionStatus = "ARCHIVE // CONNECTING TO CKAN...";
+    [ObservableProperty] private string _modFilter = "";
+    
+    partial void OnModFilterChanged(string value)
     {
-        get;
-        set
+        if (!_isLoading)
         {
-            if (field == value) return;
-            field = value; OnPropertyChanged();
+            ApplyFilters();
         }
     }
-
-    private string? NextCursor
+    
+    [ObservableProperty] private string _authorFilter = "";
+    
+    partial void OnAuthorFilterChanged(string value)
     {
-        get;
-        set
+        if (!_isLoading)
         {
-            if (field == value) return;
-            field = value; OnPropertyChanged();
+            ApplyFilters();
         }
     }
-
-    private bool HasNext
-    {
-        get;
-        set
-        {
-            if (field == value) return;
-            field = value; OnPropertyChanged();
-        }
-    }
-
-    public string ModFilter
-    {
-        get;
-        set
-        {
-            if (field == value) return;
-            field = value; OnPropertyChanged();
-        }
-    }
-
-    public string AuthorFilter
-    {
-        get;
-        set
-        {
-            if (field == value) return;
-            field = value; OnPropertyChanged();
-        }
-    }
-
-    public string ConnectionStatus
-    {
-        get;
-        set
-        {
-            if (field == value) return;
-            field = value; OnPropertyChanged();
-        }
-    } = "ARCHIVE // CONNECTING TO CKAN...";
 
     public DiscoverPageViewModel(IModApiService api)
     {
@@ -88,9 +53,32 @@ public partial class DiscoverPageViewModel : PageViewModel
     [RelayCommand]
     private async Task FetchMods()
     {
+        _isLoading = true;
         var response = await _api.GetAllModsAsync();
-        ModList = new ObservableCollection<Mod>(response);
         ConnectionStatus = "ARCHIVE // ACQUIRED CKAN DATA FEED";
+        _modList = ModListFiltered = new ObservableCollection<Mod>(response);
+        _isLoading = false;
+        ApplyFilters();
+    }
+
+    [RelayCommand]
+    private void ApplyFilters()
+    {
+        ModListFiltered = new ObservableCollection<Mod>(_modList.Where(mod =>
+        {
+            if (string.IsNullOrWhiteSpace(ModFilter) && string.IsNullOrEmpty(AuthorFilter))
+                return true;
+            
+            if (!string.IsNullOrWhiteSpace(ModFilter) && !string.IsNullOrEmpty(AuthorFilter))
+            {
+                return mod.Name.ToLower().Contains(ModFilter.ToLower()) && 
+                       mod.AuthorsDisplay.ToLower().Contains(AuthorFilter.ToLower());
+            }
+
+            return !string.IsNullOrWhiteSpace(ModFilter) ? 
+                mod.Name.ToLower().Contains(ModFilter.ToLower()) : 
+                mod.AuthorsDisplay.ToLower().Contains(AuthorFilter.ToLower());
+        }));
     }
     
     [RelayCommand(AllowConcurrentExecutions = true)]
